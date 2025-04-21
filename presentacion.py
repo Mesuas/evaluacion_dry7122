ad775a132fb5917a48825b54612f682a9d9dd4fd
NetAcad DEVASC <changeme@example.com>
2025-04-21 02:10:04 +0000

    Again

--- presentacion.py
+++ presentacion.py
@@ -1,4 +1,3 @@
 print("Evaluacion Nº1 Programación y Redes Virtualizadas")
 print("Participantes:")
 print("Mosiah Balboa")
-
--- verificacion_puerto.py
+++ verificacion_puerto.py
@@ -12,4 +12,4 @@ while True:
             print("Número fuera del rango válido de puertos.")
     except ValueError:
         print("Ingrese un numero de puerto valido") 
-        
+    
